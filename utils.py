c6744b66a898f9b6d0eb407a8cd21307e9d5722d
Arkadiy Vladimirov <ark.vladimirov@outlook.com>
2024-12-13 01:43:04 +0000

    federated ppo pipeline with pythia

--- utils.py
+++ utils.py
@@ -1,13 +1,19 @@
 import typing as tp
 
 from torch import nn
+from trl import PPOTrainer
 from transformers.modeling_outputs import CausalLMOutputWithPast
+
 
 ###############################################################################
 # Policies mixture class
 ###############################################################################
 
 class PolicyMixture(nn.Module):
+    """
+    Weighted normalized sum of policies. Policies contribute proportionally to
+    coefficients given.
+    """
     def __init__(
         self, 
         policies: tp.Sequence[nn.Module], 
@@ -26,4 +32,54 @@ class PolicyMixture(nn.Module):
         res_logits = sum(logits) / sum(self._coefs)
         
         return CausalLMOutputWithPast(logits=res_logits)
-    
+
+
+###############################################################################
+# Policy Commutator
+###############################################################################
+
+Matrix = tp.Sequence[tp.Sequence[float]]
+
+class PolicyCommutator:
+    """
+    Policy mixture dispatcher for a set of policies according to commutation
+    matrix.
+    """
+    def __init__(
+        self,
+        policies: tp.Sequence[nn.Module],
+        commutant: Matrix,
+    ) -> None:
+
+        assert len(commutant) == len(policies)
+        # warning: 2nd commutant dimension validity not ensured
+
+        self._policies = policies
+        self._commutant = commutant
+
+    def __getitem__(self, idx: int) -> PolicyMixture:
+        return self.get_reference_policy(idx)
+
+    def get_reference_policy(self, idx: int) -> PolicyMixture:
+        return PolicyMixture(self._policies, self._commutant[idx])
+
+
+###############################################################################
+# CustomPPOTrainer
+###############################################################################
+
+class CustomPPOTrainer(PPOTrainer):
+    """
+    Slightly modified PPOTrainer.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        Regular init with no cumbersome run renaming.
+        """
+
+        super().__init__(*args, **kwargs)
+
+        if hasattr(self.args, "exp_name"):
+            self.args.run_name = f"{self.args.exp_name}"
+
