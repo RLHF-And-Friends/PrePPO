3ada2207f9e565c6b53729a8ff0ef9e8608aff56
Arkadiy Vladimirov <ark.vladimirov@outlook.com>
2025-01-13 02:44:10 +0000

    ppo script: quantized models

--- scripts/ppo.py
+++ scripts/ppo.py
@@ -1,4 +1,5 @@
 import os
+import torch
 
 from datasets import load_dataset
 
@@ -10,7 +11,7 @@ from transformers import (
 
 from peft import (
     PeftModelForSequenceClassification,
-    TaskType, 
+    TaskType,
     get_peft_model,
     prepare_model_for_kbit_training,
 )
@@ -26,8 +27,7 @@ from trl.trainer.utils import peft_module_casting_to_bf16
 from fed_ppo.ppo_trainer import CustomPPOTrainer
 from fed_ppo.utils import apply_chat_template, tokenize
 from fed_ppo.prompts import (
-    STAY_WITHIN_THE_TOKEN_LIMIT, 
-    STAY_WITHIN_THE_TOKEN_LIMIT_TRAININIG_AWARE
+    STAY_WITHIN_THE_TOKEN_LIMIT,
 )
 
 
@@ -75,15 +75,15 @@ DATASET_NAME        = DATASET_PATH.split('/')[1]
 # Project name
 # =================================================================================================
 PROJECT_NAME = "Distributed-PPO"
-EXP_NAME = f"{POLICY_NAME}-1xA100-80GB-BatchSize-16-MaxTok-512"
+EXP_NAME = f"{POLICY_NAME}-Q4-4xA4000-16GB-BatchSize-64-MaxTok-512"
 
-### WandB
+# WandB
 # =================================================================================================
 os.environ["WANDB_PROJECT"] = PROJECT_NAME
 os.environ["WANDB_ENTITY"] = "RADFAN"
 
 
-### Configs
+# Configs
 
 # Policy
 # =================================================================================================
@@ -99,9 +99,9 @@ policy_model_config = ModelConfig(
     lora_target_modules  = ["q_proj", "k_proj", "v_proj", "o_proj"],
     # Quantization
     # ---------------------------------------------------------------------------------------------
+    torch_dtype          = "bfloat16", # fallback if no quantization
     load_in_8bit         = False,
     load_in_4bit         = True,
-    torch_dtype          = "bfloat16",
     bnb_4bit_quant_type  = "nf4",
     use_bnb_nested_quant = True,
 )
@@ -119,9 +119,11 @@ value_model_config = ModelConfig(
     lora_target_modules = ["q_proj", "k_proj", "v_proj", "o_proj"],
     # Quantization
     # ---------------------------------------------------------------------------------------------
+    torch_dtype         = "bfloat16", # fallback if no quantization
     load_in_8bit        = False,
-    load_in_4bit        = False,
-    torch_dtype         = "bfloat16",
+    load_in_4bit        = True,
+    bnb_4bit_quant_type  = "nf4",
+    use_bnb_nested_quant = True,
 )
 
 # Reward model
@@ -139,30 +141,35 @@ ppo_config = PPOConfig(
     exp_name            = EXP_NAME,
     output_dir          = f"~/hf/models/{PROJECT_NAME}/{EXP_NAME}",
     dataset_num_proc    = 16,
-    num_mini_batches    = 1,
-    learning_rate       = 1e-5,
-    # Make sure the desired effective batch size == batch_size * accum_steps * num_devices
-    per_device_train_batch_size = 1,
-    per_device_eval_batch_size  = 1,
-    gradient_accumulation_steps = 4,
+    # ppo epoch size = data batch size = effective ppo batch size * num mini batches
+    # effecitve ppo batch size = per_dev_tr_bs * grad_acc_st * num_dev
+    num_ppo_epochs                  = 1, # num ppo epochs per rollout batch
+    num_mini_batches                = 1, # batches in ppo epoch
+    per_device_train_batch_size     = 1, # \
+                                         #  > effective local ppo batch size
+    gradient_accumulation_steps     = 8, # /
+    local_rollout_forward_batch_size= 8, # response generation and processing batch size
+    # per_device_eval_batch_size  = 1,
     num_train_epochs    = 1,
     response_length     = 512,
     stop_token          = "eos",
     # Logging
     # ---------------------------------------------------------------------------------------------
-    save_steps          = 160, 
+    save_steps          = 640, 
     # Push to hub after training
     # ---------------------------------------------------------------------------------------------
     push_to_hub         = True,
     hub_model_id        = f"RLHF-And-Friends/{EXP_NAME}",
+    # Optimizer params
+    # ---------------------------------------------------------------------------------------------
+    learning_rate       = 1e-5,
+    adam_epsilon        = 1e-5,
     # On-policy params
     # ---------------------------------------------------------------------------------------------
-    missing_eos_penalty = 0.0,
-    local_rollout_forward_batch_size = 1,
+    missing_eos_penalty = 1.0,  # as in N+ PPO implementation details
     # PPO params
     # ---------------------------------------------------------------------------------------------
-    num_ppo_epochs      = 1,
-    whiten_rewards      = False,
+    whiten_rewards      = True, # normalize reward in batch
     kl_coef             = 0.05,
     cliprange           = 0.2,
     vf_coef             = 0.1,
@@ -190,13 +197,11 @@ if tokenizer.pad_token is None:
 # -------------------------------------------------------------------------------------------------
 sft_policy = AutoModelForCausalLM.from_pretrained(
     policy_model_config.model_name_or_path,
-    use_cache = True,
-    quantization_config = get_quantization_config(policy_model_config),
+    quantization_config=get_quantization_config(policy_model_config),
+    torch_dtype=getattr(torch, policy_model_config.torch_dtype)
 )
 sft_policy.resize_token_embeddings(len(tokenizer), mean_resizing=False)
 sft_policy.config.pad_token_id = tokenizer.pad_token_id
-peft_module_casting_to_bf16(sft_policy)
-sft_policy = prepare_model_for_kbit_training(sft_policy, use_gradient_checkpointing=False)
 
 # Policy
 # -------------------------------------------------------------------------------------------------
@@ -206,65 +211,72 @@ policy = get_peft_model(sft_policy, get_peft_config(policy_model_config))
 # -------------------------------------------------------------------------------------------------
 base_value_head_model = AutoModelForSequenceClassification.from_pretrained(
     policy_model_config.model_name_or_path,
-    num_labels = 1,
-    use_cache = True,
+    num_labels=1,
+    quantization_config=get_quantization_config(value_model_config),
+    torch_dtype=getattr(torch, value_model_config.torch_dtype)
 )
 base_value_head_model.resize_token_embeddings(len(tokenizer), mean_resizing=False)
 base_value_head_model.config.pad_token_id = tokenizer.pad_token_id
 
-# Value model
-# -------------------------------------------------------------------------------------------------
-value_model = get_peft_model(
-    base_value_head_model,
-    get_peft_config(value_model_config)
-)
-
 # Reward model
 # -------------------------------------------------------------------------------------------------
+# Load LoRA adapters for reward model
 reward_model = PeftModelForSequenceClassification.from_pretrained(
     base_value_head_model,
     reward_model_config.model_name_or_path,
 )
+# Merge adapters into the model
+reward_model = reward_model.merge_and_unload()
+
+# Value model
+# -------------------------------------------------------------------------------------------------
+# Create value model from reward model as recommended in N+ PPO impl. details
+value_model = get_peft_model(
+    reward_model,
+    get_peft_config(value_model_config)
+)
+
 
 # Data
 # =================================================================================================
 train_dataset = load_dataset(
-    DATASET_PATH, 
+    DATASET_PATH,
     split=DATASET_TRAIN_SPLIT
-).select(range(16000))
+).select(range(64000))
+
 eval_dataset = load_dataset(
-    DATASET_PATH, 
+    DATASET_PATH,
     split=DATASET_VAL_SPLIT
-).select(range(160))
+).select(range(640))
 
 train_dataset = train_dataset.remove_columns("messages")
 eval_dataset = eval_dataset.remove_columns("messages")
 
 train_dataset = train_dataset.map(
-    apply_chat_template, 
+    apply_chat_template,
     fn_kwargs={
-        "tokenizer": tokenizer, 
+        "tokenizer": tokenizer,
         "system_prompt": STAY_WITHIN_THE_TOKEN_LIMIT(512)
-    }, 
+    },
     load_from_cache_file=False
 )
 eval_dataset = eval_dataset.map(
-    apply_chat_template, 
+    apply_chat_template,
     fn_kwargs={
         "tokenizer": tokenizer,
     },
-    load_from_cache_file = False
+    load_from_cache_file=False
 )
 
 train_dataset = train_dataset.map(
-    tokenize, 
-    fn_kwargs={"tokenizer": tokenizer}, 
-    load_from_cache_file = False
+    tokenize,
+    fn_kwargs={"tokenizer": tokenizer},
+    load_from_cache_file=False
 )
 eval_dataset = eval_dataset.map(
-    tokenize, 
+    tokenize,
     fn_kwargs={"tokenizer": tokenizer},
-    load_from_cache_file = False
+    load_from_cache_file=False
 )
 
 train_dataset = train_dataset.remove_columns(["prompt", "prompt_id"])
