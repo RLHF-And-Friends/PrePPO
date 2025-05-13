23ccd03d023d3f9a260261e3cd73583bdd211378
evgurov <evgurovv@yandex.ru>
2025-05-13 20:52:01 +0300

    prepare_wiki_lingua_reward done

---
+++ src/hf_ppo/scripts/wiki_lingua/datasets/prepare_wiki_lingua_reward.py
@@ -0,0 +1,86 @@
+from datasets import (
+    load_dataset,
+    DatasetDict,
+    concatenate_datasets,
+    get_dataset_config_names
+)
+
+from hf_ppo.utils import push_dataset_to_hub_with_retries
+from hf_ppo.data_utils import cat_columns_contents
+
+
+# Load dataset
+# =================================================================================================
+
+dataset_path = "RLHF-And-Friends/wiki_lingua_preference"
+all_configs = get_dataset_config_names(dataset_path)
+
+train_subsets = []
+validation_subsets = []
+for config in all_configs:
+    # Train subset
+    # ---------------------------------------------------------------------------------------------
+    train_subset = load_dataset(dataset_path, name=config, split="train")
+    train_subset = train_subset.add_column(
+        name="language",
+        column=[config]*len(train_subset)
+    )
+    train_subset["text"] = [
+        train_text + "\n\nTL;DR: " for train_text in train_subset["text"]
+    ]
+    train_subsets.append(train_subset)
+
+    # Validation subset
+    # ---------------------------------------------------------------------------------------------
+    validation_subset = load_dataset(dataset_path, name=config, split="validation")
+    validation_subset = validation_subset.add_column(
+        name="language",
+        column=[config]*len(validation_subset)
+    )
+    validation_subset["text"] = [
+        validation_text + "\n\nTL;DR: " for validation_text in validation_subset["text"]
+    ]
+    validation_subsets.append(validation_subset)
+
+train_dataset = concatenate_datasets(train_subsets)
+validation_dataset = concatenate_datasets(validation_subsets)
+
+# Cat prompt and completions
+# =================================================================================================
+
+train_dataset = train_dataset.map(
+    cat_columns_contents,
+    fn_kwargs={
+        "lhs_column_names": ["text", "text"],
+        "rhs_column_names": ["chosen", "rejected"],
+        "cat_column_names": ["chosen", "rejected"],
+    },
+    desc = "Train dataset concatenation",
+    load_from_cache_file=False,
+)
+
+validation_dataset = validation_dataset.map(
+    cat_columns_contents,
+    fn_kwargs={
+        "lhs_column_names": ["text", "text"],
+        "rhs_column_names": ["chosen", "rejected"],
+        "cat_column_names": ["chosen", "rejected"],
+    },
+    desc = "Eval dataset concatenation",
+    load_from_cache_file=False,
+)
+
+new_dataset = DatasetDict({
+    "train": train_dataset,
+    "validation": validation_dataset,
+})
+
+# Push dataset to hub
+# =================================================================================================
+
+NEW_DATASET_NAME = "wiki-lingua-reward"
+
+push_dataset_to_hub_with_retries(
+    new_dataset,
+    repo_id=f"RLHF-And-Friends/{NEW_DATASET_NAME}",
+)
---
+++ src/hf_ppo/scripts/wiki_lingua/reward.py
@@ -0,0 +1,238 @@
+import os
+
+import torch
+
+from datasets import load_dataset
+from tokenizers import processors
+
+from transformers import (
+    AutoModelForSequenceClassification, AutoTokenizer
+)
+
+from peft import get_peft_model, TaskType, prepare_model_for_kbit_training
+
+from trl import (
+    ModelConfig,
+    RewardConfig,
+    RewardTrainer,
+    get_peft_config,
+    get_quantization_config,
+)
+
+from hf_ppo.utils import (
+    push_to_hub_with_retries
+)
+
+
+# #################################################################################################
+# NAMES & PATHS
+# #################################################################################################
+
+# Model path
+# =================================================================================================
+MODEL_PATH = "meta-llama/Llama-3.2-3B"
+MODEL_NAME = MODEL_PATH.split('/')[1]
+
+# Dataset path
+# =================================================================================================
+DATASET_PATH        = "RLHF-And-Friends/wiki-lingua-reward"
+CONFIGURATIONS      = ["en", "ru", "de", "fr", "es", "it", "nl"]
+DATASET_TRAIN_SPLIT = "train"
+DATASET_VAL_SPLIT   = "validation"
+DATASET_NAME        = DATASET_PATH.split('/')[1]
+
+# Project name
+# =================================================================================================
+PROJECT_NAME = "RM-TLDR"
+EXP_NAME = f"{MODEL_NAME}"
+
+# WandB
+# =================================================================================================
+os.environ["WANDB_PROJECT"] = PROJECT_NAME
+os.environ["WANDB_ENTITY"]  = "RADFAN"
+
+
+# #################################################################################################
+# CONFIGS
+# #################################################################################################
+
+TRAIN_SIZE  = 92858
+EVAL_SIZE   = 2000
+
+# Model config
+# =================================================================================================
+
+model_config = ModelConfig(
+    model_name_or_path   = MODEL_PATH,
+
+    # LoRA
+    # ---------------------------------------------------------------------------------------------
+    use_peft             = True,
+    lora_task_type       = TaskType.SEQ_CLS,
+    use_rslora           = False,
+    lora_r               = 16,
+    lora_alpha           = 32,
+    lora_dropout         = 0.0,
+    lora_target_modules  = ["q_proj", "k_proj", "v_proj", "o_proj"],
+    # Head will require grad automatically
+    lora_modules_to_save = None,
+
+    # Quantization
+    # ---------------------------------------------------------------------------------------------
+    load_in_8bit         = False,
+    load_in_4bit         = False,
+    bnb_4bit_quant_type  = "nf4",
+    use_bnb_nested_quant = True,
+    torch_dtype          = "bfloat16",
+)
+
+# Reward trainer config
+# =================================================================================================
+
+training_args = RewardConfig(
+    # Reward trainer params
+    # ---------------------------------------------------------------------------------------------
+    dataset_num_proc            = 16,
+    center_rewards_coefficient  = None,
+    max_length                  = 1024,
+
+    # Common
+    # ---------------------------------------------------------------------------------------------
+    run_name                    = EXP_NAME,
+    output_dir                  = f"~/hf/models/{PROJECT_NAME}/{EXP_NAME}",
+    num_train_epochs            = 1,
+    per_device_train_batch_size = 2,
+    per_device_eval_batch_size  = 2,
+    gradient_accumulation_steps = 8,
+    gradient_checkpointing      = False,
+    bf16                        = True,
+    
+    # Optimizer
+    # ---------------------------------------------------------------------------------------------
+    learning_rate = 1e-5,
+
+    # Logs
+    # ---------------------------------------------------------------------------------------------
+    logging_steps               = 20,
+    eval_strategy               = "steps",
+    eval_steps                  = 200,
+
+    # Push to hub after training
+    # ---------------------------------------------------------------------------------------------
+    push_to_hub                 = False, # push manually with pad embedding removed
+    hub_model_id                = f"RLHF-And-Friends/{PROJECT_NAME}-{EXP_NAME}",
+)
+
+# #################################################################################################
+# TOKENIZER & MODELS
+# #################################################################################################
+
+# Tokenizer
+# =================================================================================================
+
+initial_tokenizer = AutoTokenizer.from_pretrained(
+    model_config.model_name_or_path
+)
+
+tokenizer = AutoTokenizer.from_pretrained(
+    model_config.model_name_or_path,
+    pad_token = "<|pad|>",
+    add_eos_token = True, # To add EOS token during tokenization (does not work for Llama3 tokenizer)
+)
+
+# Add postrprocessor to add EOS token (Only for Llama3 model!!!)
+# -------------------------------------------------------------------------------------------------
+
+tokenizer._tokenizer.post_processor = processors.TemplateProcessing(
+    single=f"{tokenizer.bos_token}:0 $A:0 {tokenizer.eos_token}:0",
+    pair=f"{tokenizer.bos_token}:0 $A:0 {tokenizer.bos_token}:1 $B:1 {tokenizer.eos_token}:1",
+    special_tokens=[
+        (tokenizer.bos_token, tokenizer.bos_token_id),
+        (tokenizer.eos_token, tokenizer.eos_token_id),
+    ],
+)
+
+# Model
+# =================================================================================================
+
+# Make quantization config
+# -------------------------------------------------------------------------------------------------
+quantization_config = get_quantization_config(model_config)
+
+# Set model type
+# -------------------------------------------------------------------------------------------------
+torch_dtype = getattr(torch, model_config.torch_dtype)
+
+# Create model
+# -------------------------------------------------------------------------------------------------
+model = AutoModelForSequenceClassification.from_pretrained(
+    model_config.model_name_or_path,
+    num_labels = 1,
+    quantization_config = quantization_config,
+    torch_dtype = torch_dtype,
+)
+
+if model_config.load_in_4bit or model_config.load_in_8bit:
+    model = prepare_model_for_kbit_training(
+        model,
+        training_args.gradient_checkpointing,
+    )
+
+# Add padding token
+# -------------------------------------------------------------------------------------------------
+model.resize_token_embeddings(len(tokenizer), mean_resizing=False)
+model.config.pad_token_id = tokenizer.pad_token_id
+
+# Wrap in LoRA
+# -------------------------------------------------------------------------------------------------
+lora_config = get_peft_config(model_config)
+model = get_peft_model(model, lora_config)
+
+
+# #################################################################################################
+# LOAD DATASET
+# #################################################################################################
+
+dataset = load_dataset(DATASET_PATH)
+
+train_dataset = dataset["train"].select(range(TRAIN_SIZE))
+eval_dataset = dataset["validation"].select(range(EVAL_SIZE))
+
+
+# #################################################################################################
+# TRAINING
+# #################################################################################################
+
+# Train
+# =================================================================================================
+def main() -> None:
+    trainer = RewardTrainer(
+        model            = model,
+        processing_class = tokenizer,
+        args             = training_args,
+        train_dataset    = train_dataset,
+        eval_dataset     = eval_dataset,
+    )
+    trainer.train()
+
+    # Delete PAD token from the model's vocabulary
+    # ---------------------------------------------------------------------------------------------
+    trainer.model.resize_token_embeddings(len(tokenizer) - 1)
+
+    # Revert tokenizer to the initial state
+    # ---------------------------------------------------------------------------------------------
+    trainer.tokenizer = initial_tokenizer
+
+    # Merge LoRA adapters into the model
+    # ---------------------------------------------------------------------------------------------
+    trainer.model = trainer.model.merge_and_unload()
+
+    # Push model to hub
+    # ---------------------------------------------------------------------------------------------
+    push_to_hub_with_retries(trainer, DATASET_NAME)
+
+
+# Accelerate entry-point
+# =================================================================================================
+if __name__ == "__main__":
+    main()
